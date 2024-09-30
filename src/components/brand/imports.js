faea3b181b3a9b2719fbafa52df88f38c715107f
Vinilazza <108952051+Vinilazza@users.noreply.github.com>
2024-09-30 08:39:20 -0300

    u

--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -2,6 +2,6 @@ import google from "../../assets/google.png";
 import slack from "../../assets/slack.png";
 import atlassian from "../../assets/atlassian.png";
 import dropbox from "../../assets/dropbox.png";
-import shopify from "../../assets/shopfy.png";
+import shopify from "../../assets/shopify.png";
 
 export { google, slack, atlassian, dropbox, shopify };
